d89507133e9ca572c9319f2722ce976b2d8361dd
Tommy Ludwig <thomas.ludwig@rakuten.com>
2017-05-29 16:52:52 +0900

    AnimalEmojiController with findAll and findOne

---
+++ src/main/kotlin/com/example/jenkinscddemo/AnimalEmojiController.kt
@@ -0,0 +1,30 @@
+package com.example.jenkinscddemo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+/**
+ * TODO write description for class
+ *
+ * @author Tommy Ludwig
+ */
+@RestController
+class AnimalEmojiController {
+
+    final val animals: Flux<Animal> = Flux.just(Animal("elephant", "🐘"), Animal("tiger", "🐅"), Animal("koala", "🐨"))
+
+    @GetMapping("/animals")
+    fun getAllAnimals(): Flux<Animal> {
+        return animals
+    }
+
+    @GetMapping("/animals/{name}")
+    fun getSpecificAnimal(@PathVariable name: String): Mono<Animal> {
+        return animals.filter { it.name == name }.singleOrEmpty()
+    }
+}
+
+data class Animal(val name: String, val emoji: String)
